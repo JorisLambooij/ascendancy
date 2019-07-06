e47c589c8a165b23d1a6033ba9bbcacb0322a5cc
Jan Schulte-Ortbeck <JanSchulte-Ortbeck@users.noreply.github.com>
2019-07-06 17:02:51 +0200

    Minimap
    -added rudimentary Minimap
    -added resize script that doesnt work
    
    ToDo:
    -make script work
    -add Minimap layer to display units as icons:
    https://blog.theknightsofunity.com/implementing-minimap-unity/

---
+++ Ascendancy/Assets/Scripts/UI/Ingame Menu/MinimapMinMax.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapMinMax : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void ToggleMinimapSize()
+    {
+        GameObject minimapImage;
+
+        void Start()
+        {
+            minimapImage = GameObject.Find("Image_Minimap");
+        }
+
+        void ToggleMinimapSize()
+        {
+            minimapImage.transform.position = new Vector3(100, 200, 100); //ToDo: Resize Properly, check toggle
+        }
+    }
+}
