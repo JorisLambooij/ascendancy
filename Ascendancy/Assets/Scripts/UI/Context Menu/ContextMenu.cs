ce3a0791475705b4d3ffae0c2bb4e635568fbfa4
Jan Schulte-Ortbeck <JanSchulte-Ortbeck@users.noreply.github.com>
2019-07-08 22:01:05 +0200

    Context Menu added (broken af)

---
+++ Ascendancy/Assets/Scripts/UI/Context Menu/ContextMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ContextMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //purging transparent areas of buttons
+        Image[] images = transform.GetComponentsInChildren<Image>();
+
+        foreach (Image img in images)
+        {
+            img.alphaHitTestMinimumThreshold = 0.5f;
+        }
+        Debug.Log("fixed it!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
