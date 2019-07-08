7c4edefc73442374e7b745297a48457097f4407b
Jan Schulte-Ortbeck <JanSchulte-Ortbeck@users.noreply.github.com>
2019-07-08 20:43:30 +0200

    FIxed Main Menu
    Main menu, fixed small stuff

---
+++ Ascendancy/Assets/Scripts/UI/Main Menu/Mmenu_ButtonScipt1.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Mmenu_ButtonScipt1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void BT1_click()
+    {
+        SceneManager.LoadScene("Development");
+    }
+
+    public void BT2_click()
+    {
+
+    }
+
+    public void BT3_click()
+    {
+
+    }
+
+    public void BT4_click()
+    {
+
+    }
+
+    public void BT5_click()
+    {
+
+    }
+
+    public void BT6_click()
+    {
+
+    }
+
+    public void BT7_click()
+    {
+
+    }
+
+    public void BT8_click()
+    {
+        Application.Quit();
+    }
+}
