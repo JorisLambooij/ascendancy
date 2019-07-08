88a9fd899b8a7c235a2028347d6d735d0009edba
Unknown <joris@lambooy.nl>
2019-07-08 14:08:44 +0200

    Buildings WIP

---
+++ Ascendancy/Assets/Scripts/Buildings/BuildingFeatures/RecruitmentFeature.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecruitmentFeature : BuildingFeature
+{
+    public List<UnitInfo> recuitableUnits;
+
+    /// <summary>
+    /// Recruits a specific Unit.
+    /// </summary>
+    /// <param name="unitInfo">The Unit we wish to spawn.</param>
+    /// <returns>True on a success, false otherwise.</returns>
+    bool Recruit(UnitInfo unitInfo)
+    {
+        // if unit is not allowed, abort
+        if (!recuitableUnits.Contains(unitInfo))
+            return false;
+
+        // enough resources?
+
+        Unit newUnit;
+        return true;
+    }
+}
---
+++ Ascendancy/Assets/Scripts/Buildings/BuildingInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewBuilding", menuName = "Building SO", order = 2)]
+public class BuildingInfo : ScriptableObject
+{
+    public string buildingName;
+
+    public float goldCost;
+
+    public List<BuildingFeature> features;
+}
