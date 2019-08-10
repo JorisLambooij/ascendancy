8107d78c5248a09574be3c17ae6ee3ac0f606c07
Jan Schulte-Ortbeck <JanSchulte-Ortbeck@users.noreply.github.com>
2019-08-10 23:20:15 +0200

    Added Button to toggle grid in map editor (without function yet)

---
+++ Ascendancy/Assets/Scripts/UI/MapEditor/OptButtonGridClick.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptButtonGridClick : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void OnClick()
+    {
+        Debug.Log("TODO: ENABLE GRID");
+    }
+}
