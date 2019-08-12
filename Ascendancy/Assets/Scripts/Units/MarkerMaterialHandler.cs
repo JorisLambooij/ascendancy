4eb13a7f7485b801ba41d50fdf5e751fb40dbed1
Jan Schulte-Ortbeck <JanSchulte-Ortbeck@users.noreply.github.com>
2019-08-12 22:31:59 +0200

    Added minimap markers for Dummy Tanks

---
+++ Ascendancy/Assets/Scripts/Units/MarkerMaterialHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarkerMaterialHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpriteRenderer sRenderer = GetComponent<SpriteRenderer>();
+
+        Entity e = transform.GetComponentInParent<Entity>();
+        if (e == null)
+            e = transform.parent.GetComponentInParent<Entity>();
+
+        Debug.Log(e.Owner.name);
+        Debug.Log(e.Owner.playerColor);
+
+        Color c = e.Owner.playerColor;
+
+        sRenderer.color = c;
+    }
+
+}
