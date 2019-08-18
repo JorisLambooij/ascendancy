d5acdf5eb02b605ce2fb893642d4354fef77ea97
Jan Schulte-Ortbeck <JanSchulte-Ortbeck@users.noreply.github.com>
2019-08-18 21:38:18 +0200

    changes to the FoW system

--- Ascendancy/Assets/Scripts/Terrain/FogOfWarHandler.cs
+++ Ascendancy/Assets/Scripts/Terrain/FogOfWarHandler.cs
@@ -6,10 +6,6 @@ public class FogOfWarHandler : MonoBehaviour
 {
     public MeshFilter terrainMeshFilter;
     public MeshFilter fogMeshFilter;
-    public World world;
-
-    private enum Cellstate {UNKNOWN, KNOWN, VISIBLE}
-    private Cellstate[,] mapState;
 
     // Start is called before the first frame update
     void Start()
@@ -20,16 +16,8 @@ public class FogOfWarHandler : MonoBehaviour
         if (terrainMeshFilter == null)
             Debug.LogError("Fog of War MeshFilter not linked. Fog of War will be disabled.");
 
-        if (world == null)
-            world = GetComponentInParent<World>();
-
-        if (world == null)
-            Debug.LogError("Fog of War World not linked. Fog of War will be disabled.");
-
         fogMeshFilter.mesh = terrainMeshFilter.mesh;
         fogMeshFilter.transform.position += new Vector3(0, 5, 0);
-
-        mapState = new Cellstate[world.worldSize, world.worldSize];
     }
 
     // Update is called once per frame
