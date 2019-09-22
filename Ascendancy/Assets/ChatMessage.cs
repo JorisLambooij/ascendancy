7b22137a02644d69f028616da6394fc4a9f5f01c
Unknown <joris@lambooy.nl>
2019-09-22 16:38:21 +0200

    Scrolling in chat works as intended (i think)

---
+++ Ascendancy/Assets/ChatMessage.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChatMessage : MonoBehaviour, PropertySubscriber<int>
+{
+    public Text textContent;
+
+    public int index { get; set; }
+    public MessageWindow messageWindow { get; set; }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        textContent = GetComponent<Text>();
+    }
+
+    public void Callback(int value)
+    {
+        if (value > index)
+        {
+            StartCoroutine(DestroyThis());
+        }
+    }
+
+    IEnumerator DestroyThis()
+    {
+        yield return new WaitForEndOfFrame();
+        messageWindow.currentMessageTimestamp.Unsubscribe(this);
+        Destroy(this.gameObject);
+        messageWindow.ScrollToBottom();
+    }
+}
--- Ascendancy/Assets/Scripts/UI/Lobby/MessageWindow.cs
+++ Ascendancy/Assets/Scripts/UI/Lobby/MessageWindow.cs
@@ -5,17 +5,57 @@ using UnityEngine.UI;
 
 public class MessageWindow : MonoBehaviour
 {
+    public int maximumNumberOfMessages;
     public Text messageWindowText;
+    public Transform chatWindow;
+    public GameObject messagePrefab;
+    public ScrollRect scrollRect;
+
+    public SubscribableProperty<int> currentMessageTimestamp;
+
+    private bool gluedToBottom;
+    private const float epsilon = 0.01f;
 
     void Start()
     {
+        gluedToBottom = true;
+        currentMessageTimestamp = new SubscribableProperty<int>(0);
+
         ReceiveMessage("SYSTEM", "Lobby created");
+
+        for (int i = 0; i < 25; i++)
+            ReceiveMessage("TEST", i.ToString());
     }
 
     public void ReceiveMessage(string sender, string message)
     {
+        currentMessageTimestamp.Value++;
+
         message = sender + " [" + System.DateTime.Now.Hour.ToString("00") + ":" + System.DateTime.Now.Minute.ToString("00") + "]: " + message;
-        messageWindowText.text = messageWindowText.text + "\n" + message;
+        ChatMessage chatMessage = Instantiate(messagePrefab, chatWindow).GetComponent<ChatMessage>();
+        chatMessage.textContent.text = message;
+        chatMessage.index = currentMessageTimestamp.Value + maximumNumberOfMessages;
+        chatMessage.messageWindow = this;
+        currentMessageTimestamp.Subscribe(chatMessage);
+        
+        gluedToBottom = scrollRect.verticalNormalizedPosition < epsilon;
+        if (gluedToBottom)
+            ScrollToBottom();
     }
 
+    public void ScrollToBottom()
+    {
+        StartCoroutine(ScrollToBottomCoroutine());
+    }
+
+    private IEnumerator ScrollToBottomCoroutine()
+    {
+        yield return new WaitForEndOfFrame();
+        scrollRect.verticalNormalizedPosition = 0;
+    }
+
+    public void OnScroll()
+    {
+        gluedToBottom = scrollRect.verticalNormalizedPosition < epsilon;
+    }
 }
