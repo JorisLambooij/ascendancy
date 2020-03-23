f9187a8efb3cc7406ec96be530f328e72dd5684e
Jan Schulte-Ortbeck <JanSchulte-Ortbeck@users.noreply.github.com>
2020-03-23 19:07:51 +0100

    fixed mesh not displaying
    -bugfix via hybrid renderer rollback to 0.3.3
    -added cube with zombie texture as per tutorial

---
+++ Fluid Wars/Fluid Wars/Assets/Scripts/COM_Level.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+public struct COM_Level : IComponentData
+{
+    public float level;
+}
---
+++ Fluid Wars/Fluid Wars/Assets/Scripts/SYS_LevelUp.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+public class SYS_LevelUp : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        Entities.ForEach((ref COM_Level levelComponent) =>
+        {
+            levelComponent.level += 1f * Time.DeltaTime;
+        }
+        );
+    }
+}
