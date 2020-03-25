a2e50b31cc166d17fa91cd21c9362e0e73bba17a
Jan Schulte-Ortbeck <JanSchulte-Ortbeck@users.noreply.github.com>
2020-03-25 15:06:11 +0100

    Minor additions to testArena

---
+++ Fluid Wars/Fluid Wars/Assets/Scripts/GameSceneEntityManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Collections;
+
+public class GameSceneEntityManager : MonoBehaviour
+{
+    void Start()
+    {
+        EntityManager entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+
+        EntityArchetype dotEntityArchetype = entityManager.CreateArchetype(
+            typeof(COM_Position),   //position in 2D space
+            typeof(COM_PlayerInfo)  //info about owner
+            );
+
+        NativeArray < Entity > dotEntityArray = new NativeArray<Entity>(1, Allocator.Temp);
+        entityManager.CreateEntity(dotEntityArchetype, dotEntityArray);
+
+        for (int i = 0; i < dotEntityArray.Length; i++)
+        {
+            Entity dotEntity = dotEntityArray[i];
+
+            entityManager.SetComponentData(dotEntity, new COM_Position
+            {
+                x = Random.Range(-50, 50),
+                y = Random.Range(-50, 50)
+        });
+
+            //strange fuckery happens below
+
+            //entityManager.SetSharedComponentData<COM_PlayerInfo>(dotEntity, new COM_PlayerInfo
+            //{
+            //    playerID = 3,
+            //    playerColor = Color.green
+            //});
+        }
+
+        dotEntityArray.Dispose();
+    }
+}
