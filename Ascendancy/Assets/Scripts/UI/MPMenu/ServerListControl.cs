5d66a194978305e0959d85be0d4fde710cae828f
Jan Schulte-Ortbeck <JanSchulte-Ortbeck@users.noreply.github.com>
2020-04-19 17:54:12 +0200

    reworked mpmenu with new server data

--- Ascendancy/Assets/Scripts/UI/MPMenu/ServerListControl.cs
+++ Ascendancy/Assets/Scripts/UI/MPMenu/ServerListControl.cs
@@ -6,18 +6,23 @@ public class ServerListControl : MonoBehaviour
 {
     [SerializeField]
     private GameObject buttonTemplate;
+    [SerializeField]
+    private bool generateDummyEntries; 
 
     List<GameObject> items;
-    List<ServerInfo> serverList;
+    List<MPMenu_ServerResponse> serverList;
 
     private int maxServerNameLength = 20;
 
     void Start()
     {
         items = new List<GameObject>();
-        serverList = new List<ServerInfo>();
+        serverList = new List<MPMenu_ServerResponse>();
 
-        GenerateDummyEntries();
+        if (generateDummyEntries)
+            GenerateDummyEntries();
+        
+
         RefreshItems();
     }
 
@@ -26,21 +31,35 @@ public class ServerListControl : MonoBehaviour
         //TODO Refresh Items every X seconds (10? 20?)
     }
 
+    public static long RandomLong()
+    {
+        int value1 = UnityEngine.Random.Range(int.MinValue, int.MaxValue);
+        int value2 = UnityEngine.Random.Range(int.MinValue, int.MaxValue);
+        return value1 + ((long)value2 << 32);
+    }
+
     public void GenerateDummyEntries()
     {
         for (int i = 0; i < 20; i++)
         {
-            ServerInfo servInf = new ServerInfo
+            MPMenu_ServerResponse servInf = new MPMenu_ServerResponse
             {
-                ServerName = "SERVER " + i,
-                Latency = i*5,
-                Password = (i%3 == 0),
-                PlayerCountCurrent = i,
-                PlayerCountMax = 20,
-                IPAddress = "127.0.0.1"
-            };
+                nameServer = "SERVER " + i,
+                nameHost = "HOSTNAME",
+                ping = i*5,
+                passwordProtection = false,
+                playerCount = i,
+                playerMax = 20,
+                uri = new System.Uri("http://www.test.com/"),
+                serverId = RandomLong()
+        };
             serverList.Add(servInf);
         }
+    }
+
+    public void AddEntry(MPMenu_ServerResponse responseData)
+    {
+        serverList.Add(responseData);
     }
 
     public void RefreshItems()
@@ -51,28 +70,28 @@ public class ServerListControl : MonoBehaviour
         }
 
         string itemText = "";
-        foreach (ServerInfo serverData in serverList)
+        foreach (MPMenu_ServerResponse serverData in serverList)
         {
             GameObject button = Instantiate(buttonTemplate) as GameObject;
             button.SetActive(true);
 
-            itemText += serverData.ServerName.PadRight(maxServerNameLength).Substring(0, maxServerNameLength);
+            itemText += serverData.nameServer.PadRight(maxServerNameLength).Substring(0, maxServerNameLength);
 
             itemText += "\t"; //tabstop
 
             itemText += "          ";   //10 blank spaces
-            itemText += ("Connection: " + serverData.Latency + "%").PadRight(20);
+            itemText += ("Connection: " + serverData.ping + "%").PadRight(20);
 
             itemText += "\t"; //tabstop
 
-            if (serverData.Password)
+            if (serverData.passwordProtection)
                 itemText += "password".PadRight(20);
             else
                 itemText += "no password".PadRight(20);
 
             itemText += "\n";   //newline
 
-            itemText += "Players: " + serverData.PlayerCountCurrent + "/" + serverData.PlayerCountMax;
+            itemText += "Players: " + serverData.playerCount + "/" + serverData.playerMax;
 
 
             button.GetComponent<ServerListItem>().SetText(itemText);
