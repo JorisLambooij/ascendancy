fe5da39aba4ae302080af834591964264b436eae
Jan Schulte-Ortbeck <JanSchulte-Ortbeck@users.noreply.github.com>
2020-10-29 18:00:58 +0100

    Added cloud shadows

--- Ascendancy/Assets/Scripts/World/Terrain/World.cs
+++ Ascendancy/Assets/Scripts/World/Terrain/World.cs
@@ -455,7 +455,7 @@ public class World : MonoBehaviour_Singleton
                     }
                 }
             }
-            chunks[0, 0].GetComponent<MeshRenderer>().sharedMaterial.SetTexture("_TerrainTexture", TerrainTexture);
+            chunks[0, 0].GetComponent<MeshRenderer>().sharedMaterial.SetTexture("_TerrainTexture", tilemapTexture);
         }
     }
 
