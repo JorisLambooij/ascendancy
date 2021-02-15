c3525f44d230a40e5b7c165738b0f2b47d830bdb
Jan Schulte-Ortbeck <JanSchulte-Ortbeck@users.noreply.github.com>
2021-02-16 00:29:50 +0100

    Added ContextMenu and animated ESV landing is used
    TODO:
    -stop ESV from glitching through terrain

--- Ascendancy/Assets/Scripts/Entities/EntityFeatures/BuildingConversionFeature.cs
+++ Ascendancy/Assets/Scripts/Entities/EntityFeatures/BuildingConversionFeature.cs
@@ -7,9 +7,48 @@ using UnityEngine;
 public class BuildingConversionFeature : EntityFeature
 {
     public EntityInfo convertedEntity;
+    public string conversionAnimation;
+    public float conversionDelay = 0f;
 
     public override void ContextMenuOption()
     {
+        float delay = 0f;
+        //play animation first if not null
+        if (conversionAnimation != "")
+        {
+            Animator animator = entity.GetComponentInChildren<Animator>();
+
+            animator.Play(conversionAnimation);
+            foreach (AnimationClip clip in animator.runtimeAnimatorController.animationClips)
+            {
+                if (clip.name == conversionAnimation)
+                {
+                    delay = clip.length;
+                }
+            }
+
+            if (delay == 0f)
+            {
+                Debug.LogError("ERROR: Animation '" + conversionAnimation + "' not found or has length 0!");
+            }
+        }
+
+        delay += conversionDelay;
+
+        if (delay > 0f)
+        {
+            entity.StartCoroutine(DoAfterAnimation(delay));
+        }
+        else
+        {
+            DoAfterAnimation(0f);
+        }
+    }
+
+    IEnumerator DoAfterAnimation(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
         //GameManager.Instance.SwitchToMode(ControlModeEnum.buildingMode);
         BuildingPlacementMode buildingMode = GameManager.Instance.controlModeDict[ControlModeEnum.buildingMode] as BuildingPlacementMode;
         buildingMode.Building = convertedEntity;
