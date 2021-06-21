ae36ffa118aa9426cb32e5dd815af540ade89dfb
Unknown <joris@lambooy.nl>
2021-06-21 22:14:28 +0200

    Added FOW to Water Plane, additional Fog Plane, proper Gaussian Blur
    Still WIP

--- Ascendancy/Assets/Terrain/FOW/FOW_Script.cs
+++ Ascendancy/Assets/Terrain/FOW/FOW_Script.cs
@@ -5,10 +5,23 @@ using UnityEngine.Rendering;
 
 public class FOW_Script : MonoBehaviour
 {
+    enum DownSampleMode { Off, Half, Quarter }
+
+    [SerializeField]
+    DownSampleMode _downSampleMode = DownSampleMode.Quarter;
+
     public RenderTexture visibilityRT;
     public RenderTexture discoveryMap;
+    public RenderTexture discoveryMapBlurred;
 
-    public Material shaderMat;
+    public Material discoveryBlendMat;
+
+    [SerializeField, Range(0, 8)]
+    int blurStrength;
+
+    [SerializeField]
+    Shader blurShader;
+    Material _material;
 
     // Start is called before the first frame update
     void Start()
@@ -21,6 +34,49 @@ public class FOW_Script : MonoBehaviour
     void Update()
     {
         // Applies a Max-Blend Shader, i.e. the current visibility is added to the already discovered areas.
-        Graphics.Blit(visibilityRT, discoveryMap, shaderMat);
+        Graphics.Blit(visibilityRT, discoveryMap, discoveryBlendMat);
+
+        Blur(discoveryMap, discoveryMapBlurred);
+    }
+
+    void Blur(RenderTexture source, RenderTexture destination)
+    {
+        if (_material == null)
+        {
+            _material = new Material(blurShader);
+            _material.hideFlags = HideFlags.HideAndDontSave;
+        }
+
+        RenderTexture rt1, rt2;
+
+        if (_downSampleMode == DownSampleMode.Half)
+        {
+            rt1 = RenderTexture.GetTemporary(source.width / 2, source.height / 2);
+            rt2 = RenderTexture.GetTemporary(source.width / 2, source.height / 2);
+            Graphics.Blit(source, rt1);
+        }
+        else if (_downSampleMode == DownSampleMode.Quarter)
+        {
+            rt1 = RenderTexture.GetTemporary(source.width / 4, source.height / 4);
+            rt2 = RenderTexture.GetTemporary(source.width / 4, source.height / 4);
+            Graphics.Blit(source, rt1, _material, 0);
+        }
+        else
+        {
+            rt1 = RenderTexture.GetTemporary(source.width, source.height);
+            rt2 = RenderTexture.GetTemporary(source.width, source.height);
+            Graphics.Blit(source, rt1);
+        }
+
+        for (var i = 0; i < blurStrength; i++)
+        {
+            Graphics.Blit(rt1, rt2, _material, 1);
+            Graphics.Blit(rt2, rt1, _material, 2);
+        }
+
+        Graphics.Blit(rt1, destination);
+
+        RenderTexture.ReleaseTemporary(rt1);
+        RenderTexture.ReleaseTemporary(rt2);
     }
 }
