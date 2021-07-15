c30ac225c72f0f3f8004d92a787f5bdffb25cd40
Jan Schulte-Ortbeck <50276330+JanSchulte-Ortbeck@users.noreply.github.com>
2021-07-16 00:28:49 +0200

    Improvements to the custom entity editor

--- Ascendancy/Assets/Scripts/Editor/UnitEditor/UnitDetails_Editor.cs
+++ Ascendancy/Assets/Scripts/Editor/UnitEditor/UnitDetails_Editor.cs
@@ -1,26 +1,23 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
+﻿using UnityEditor;
 using UnityEngine;
 
 #if (UNITY_EDITOR)
 public class UnitDetails_Editor : EditorWindow
 {
     SerializedObject info;
+    Editor gameObjectEditor;
+    Vector2 scrollPosition;
+    bool refreshPreview = false;
 
     private static void OpenWindow()
     {
         UnitDetails_Editor window = GetWindow<UnitDetails_Editor>();
     }
 
-    private void OnEnable()
-    {
-
-    }
 
     void OnGUI()
     {
-        #region button row
+        #region button row       
         GUILayout.BeginHorizontal();
         if (GUILayout.Button("Test", GUILayout.Width(60)))
         {
@@ -29,7 +26,11 @@ public class UnitDetails_Editor : EditorWindow
         GUILayout.EndHorizontal();
         #endregion
 
+        scrollPosition = GUILayout.BeginScrollView(scrollPosition);
+
+
         GUILayout.BeginVertical(GUILayout.Width(600));
+
 
         if (info != null)
         {
@@ -47,18 +48,63 @@ public class UnitDetails_Editor : EditorWindow
                     if (sp.name == "m_Script")
                     {
                     }
+                    else if (sp.name == "prefab")
+                    {
+                        GameObject gameObject = (GameObject)sp.objectReferenceValue;
+
+                        //if (gameObject != null)
+                        //{
+                        GUILayout.BeginHorizontal();
+
+                        EditorGUI.BeginChangeCheck();
+                        EditorGUILayout.PropertyField(sp, true, GUILayout.Width(550f));
+                        if (EditorGUI.EndChangeCheck())
+                        {
+                            refreshPreview = true;
+                        }
+                        
+
+                        if (gameObjectEditor == null)
+                        {
+                            gameObjectEditor = Editor.CreateEditor(gameObject);
+                            Debug.Log("3D preview created");
+                        }
+                        else if (refreshPreview)
+                        {
+                            gameObjectEditor = Editor.CreateEditor(gameObject);
+                            refreshPreview = false;
+                            Debug.Log("3D preview refreshed");
+                        }
+
+                        if (gameObject != null)
+                            gameObjectEditor.OnPreviewGUI(GUILayoutUtility.GetRect(50, 50), EditorStyles.whiteLabel);
+                        else
+                        { 
+                        }
+                        GUILayout.EndHorizontal();
+                        //}
+                    }
+                    else if (sp.name == "thumbnail")
+                    {
+                        GUILayout.BeginHorizontal();
+                        //sp.objectReferenceValue = EditorGUI.ObjectField(GUILayoutUtility.GetRect(50, 50), sp.objectReferenceValue, typeof(Texture2D), false);
+                        sp.objectReferenceValue = EditorGUILayout.ObjectField("Thumbnail", sp.objectReferenceValue, typeof(Sprite), false);
+                        GUILayout.EndHorizontal();
+
+
+                    }
                     else
                     {
                         EditorGUILayout.PropertyField(sp, true);
-                        Debug.Log(sp.name);
                     }
             }
-
-            //info.ApplyModifiedProperties();
         }
 
         info.ApplyModifiedProperties();
+        GUILayout.EndScrollView();
         GUILayout.EndVertical();
+
+
 
         EditorGUIUtility.ExitGUI();
     }
@@ -70,6 +116,7 @@ public class UnitDetails_Editor : EditorWindow
         window.titleContent = new GUIContent(info.FindProperty("name").stringValue);
         window.maxSize = new Vector2(800f, 800f);
         window.minSize = window.maxSize;
+        refreshPreview = true;
     }
 }
 #endif
