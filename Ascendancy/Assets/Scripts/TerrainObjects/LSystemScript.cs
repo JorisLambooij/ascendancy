55a128eaa1c3b3f2b589670c43e53aa0e64f627d
Jan Schulte-Ortbeck <janschulteortbeck@gmail.com>
2021-08-08 00:13:09 +0200

    L-Trees v 0.1

--- Ascendancy/Assets/Scripts/TerrainObjects/LSystemScript.cs
+++ Ascendancy/Assets/Scripts/TerrainObjects/LSystemScript.cs
@@ -10,7 +10,9 @@ public class LSystemScript : MonoBehaviour
     [SerializeField] private float startSize = 1f;
     [SerializeField] private float length = 10f;
     [SerializeField] private float angle = 30f;
+    [SerializeField] private GameObject stem;
     [SerializeField] private GameObject branch;
+    [SerializeField] private GameObject leafs;
 
     [SerializeField] private string axiom = "X";
     [SerializeField] private List<LSystemRule> rulesInput;
@@ -73,33 +75,76 @@ public class LSystemScript : MonoBehaviour
         Vector3 branchPosition = transform.position;
         Quaternion branchOrientation = transform.rotation;
 
+        Vector3 targetPosition;
+        GameObject treeSegment;
+        Vector3 scaler;
+        float scaleFactor = 0.2f;
+
         foreach (char c in currentString)
         {
+            if (scaleFactor > 0f)
+                scaleFactor -= 0.0005f;
+
             switch (c)
             {
-                case 'F':   //draw line
-                    Vector3 targetPosition = branchPosition + (branchOrientation * transform.up) * length;
+                case 'F':   //leafs
+                    targetPosition = branchPosition + (branchOrientation * transform.up) * length;
 
-                    GameObject treeSegment = Instantiate(branch);
+                    treeSegment = Instantiate(leafs);
                     treeSegment.transform.parent = this.transform;
-                    //LineRenderer lineRenderer = treeSegment.GetComponent<LineRenderer>();
-                    //lineRenderer.SetPosition(0, initialPosition);
-                    //lineRenderer.SetPosition(1, transform.position);
-                    //lineRenderer.startWidth = currentSize;
-                    //lineRenderer.endWidth = currentSize;
 
-                    Vector3 scaler = treeSegment.transform.localScale;
-                    scaler.y = Vector3.Distance(branchPosition, targetPosition) / 2;
+                    scaler = treeSegment.transform.localScale;
+                    float scale = Vector3.Distance(branchPosition, targetPosition) / 1.5f;
+                    
+                    scaler.y = scale;
+
+                    scaler.x = scaleFactor;
+                    scaler.z = scaleFactor;
+
                     treeSegment.transform.localScale = scaler;
                     treeSegment.transform.rotation = branchOrientation;
-                    
+
                     treeSegment.transform.position = branchPosition;        // place bond here
                     //treeSegment.transform.LookAt(targetPosition);            // aim bond at atom
                     branchPosition = targetPosition;
+                    break;
+                case 'S':   //generate stem
+                    targetPosition = branchPosition + (branchOrientation * transform.up) * length;
 
+                    treeSegment = Instantiate(stem);
+                    treeSegment.transform.parent = this.transform;
 
-            break;
+                    scaler = treeSegment.transform.localScale;
+                    scaler.y = Vector3.Distance(branchPosition, targetPosition) / 1.5f;
+
+                    scaler.x = scaleFactor;
+                    scaler.z = scaleFactor;
+
+                    treeSegment.transform.localScale = scaler;
+                    treeSegment.transform.rotation = branchOrientation;
+
+                    treeSegment.transform.position = branchPosition;        // place bond here
+                    branchPosition = targetPosition;
+                    break;
                 case 'X':   //generate more 'F's
+                    break;
+                case 'G':   //generate branches
+                    targetPosition = branchPosition + (branchOrientation * transform.up) * length;
+
+                    treeSegment = Instantiate(branch);
+                    treeSegment.transform.parent = this.transform;
+
+                    scaler = treeSegment.transform.localScale;
+                    scaler.y = Vector3.Distance(branchPosition, targetPosition) / 1.5f;
+
+                    scaler.x = scaleFactor;
+                    scaler.z = scaleFactor;
+
+                    treeSegment.transform.localScale = scaler;
+                    treeSegment.transform.rotation = branchOrientation;
+
+                    treeSegment.transform.position = branchPosition;        // place bond here
+                    branchPosition = targetPosition;
                     break;
                 case '+':   //rotate clockwise Up/Down
                     branchOrientation *= Quaternion.AngleAxis(angle, transform.right);
